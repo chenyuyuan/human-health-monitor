5c640a6dd3ee91a691636920408b1ac4e2e3c501
chenyuyuan <1695215206@qq.com>
2020-01-01 16:35:53 +0800

    move startup.sh file

--- humanhealthmonitor/startup.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-nohup java -jar /root/yuan/human-health-monitor/humanhealthmonitor/target/humanhealthmonitor-0.0.1-SNAPSHOT.war >/root/yuan/log/humanhealthmonitor-0.0.1-SNAPSHOT.out 2>&1 &
