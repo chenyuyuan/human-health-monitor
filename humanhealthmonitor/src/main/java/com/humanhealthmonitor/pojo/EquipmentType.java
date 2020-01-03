15ccc2d1001e2bc75f2141bb239cb6d4443d4822
chenyuyuan <1695215206@qq.com>
2020-01-03 14:48:47 +0800

    userinfohistory

--- humanhealthmonitor/src/main/java/com/humanhealthmonitor/pojo/EquipmentType.java
+++ humanhealthmonitor/src/main/java/com/humanhealthmonitor/pojo/EquipmentType.java
@@ -7,6 +7,7 @@ public class EquipmentType {
     private int indexTotal;//测量的指标总个数
     private int alarmSerialMax;//警报界限总数
     private String introduction;//该类型设备简介
+    private String eqpTypeCHName;//设备中文名
 
 
     public String getEqpType() {
@@ -47,5 +48,13 @@ public class EquipmentType {
 
     public void setIntroduction(String introduction) {
         this.introduction = introduction;
+    }
+
+    public String getEqpTypeCHName() {
+        return eqpTypeCHName;
+    }
+
+    public void setEqpTypeCHName(String eqpTypeCHName) {
+        this.eqpTypeCHName = eqpTypeCHName;
     }
 }
